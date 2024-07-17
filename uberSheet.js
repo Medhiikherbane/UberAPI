57dae39b5843afda1c71d5fa56d21272d77f01cb
Sébastien GANDOIN <sebastien_gandoin@yahoo.fr>
2024-07-17 16:40:39 +0200

    ajout description

--- uberSheet.js
+++ uberSheet.js
@@ -2,12 +2,14 @@ const uberSheet = [
     {
       id: 1,
       name: "Fraise tagadux on the flux",
+      description: "Ce bonbon vous fera planer sur un nuage de saveurs fruitées et sucrées.",
       image:
         "https://i.pinimg.com/474x/ee/d8/79/eed8796c22744c03622f75b16b5ac6c0.jpg",
     },
     {
       id: 2,
       name: "Napoli de la mama",
+      description: "Ce gâteau vous envoie au septième ciel avec ses saveurs italiennes envoûtantes et addictives.",
       image:
         "https://i.pinimg.com/564x/e9/d9/ea/e9d9eaaedbfd6b10c91f0a4a5f55a69d.jpg",
     
@@ -15,39 +17,59 @@ const uberSheet = [
     {
     id: 3,
       name: "Zinzin de l'espace",
+      description: "Ce bonbon te donne le QI d'Etno ou de Bud selon les arrivages.",
       image:
         "https://www.bofrost.fr/medias/W1440xH1200R1.2-02065-FR-fusees-a-l-eau-pic1.jpg?context=bWFzdGVyfHByb2R1Y3QtaW1hZ2VzfDE4MjMyMjd8aW1hZ2UvanBlZ3xhRGd5TDJnME1TODVPVGczTmpnd01UQTROVGMwTDFjeE5EUXdlRWd4TWpBd1VqRXVNbDh3TWpBMk5WOUdVbDltZFhObFpYTXRZUzFzTFdWaGRWOXdhV014TG1wd1p3fDNjY2U0OTAyNDAxMTVjOTJhMjRlNmZiODBmYWM1MzBiZjQ0NTcxNmRjNjJmZGMxNzgzNDdlN2Q4ZTU4YjQ2ODY",
     },
     {
     id: 4,
         name: "Assis-toi faut que j'te parle ",
+        description: "J'ai passé ma soirée a boire. Punk, j'la sens pas, j'le sais pas, prends donc cela.",
         image:
             "https://i.pinimg.com/564x/09/bb/f3/09bbf3c9d9598382a95785f85b3c5e6f.jpg",
     },
     {
     id: 5,
         name: "R'garde la vie en bleu",
+        description: "Pour tous ceux qui en ont marre de voir la vie en rose.",
         image:
             "https://bonbons-family.com/cdn/shop/files/schtroumpfsharibo.webp?v=1684509959"
     },
     {
     id: 6,
         name: "Les billes du bonheur",
+        description: "Ces douceurs vous emporteront vers un état de béatitude avec des explosions de saveurs.",
         image:
             "https://www.generation-souvenirs.com/28630-product_large/dragibus-haribo.jpg"
     },
     {
     id: 7,
-        name: "Tic Tac BOOM",
+        name: "Tic-Tac BOOM",
+        description: "Le tic-tac qui t'explose la tête.",
         image:
            "https://www.valgourmand.com/36232-superlarge_default/tic-tac-cherry-sour-18g.webp"
     },
     {
     id: 8,
         name: "Cosmonest",
+        description: "Ce délicieux gâteau vous offrira un aller simple pour l'espace.",
         image:
            "https://img.freepik.com/photos-premium/gateau-fusee-dessus-fusee-dessus_900370-21704.jpg"
     },
+    {
+    id: 9,
+        name: "8.6",
+        description: "Tout bon punk à chien se doit d'avoir une 8.6 ou deux dans sa poche.",
+        image:
+           "https://www.cdiscount.com/pdt2/7/9/9/1/700x700/auc2008697458799/rw/biere-8-6-blonde-original-50-cl-x24.jpg"
+    },
+    {
+      id: 10,
+          name: "Delirium",
+          description: "Pour éviter les effets néfastes du manque d'alcool.",
+          image:
+             "https://unepetitemousse.fr/media/cache/sylius_shop_product_original/1c/8f/f657d0f0060895d4700f09d931d3.png"
+      },
   ];
   
   module.exports = uberSheet;
